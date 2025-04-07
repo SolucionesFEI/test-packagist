1d5c725ba9adb79cc0ee2d83f0118b5dd4f31652
Pablo Wakonigg <pablowako@gmail.com>
2025-04-07 15:32:30 +0200

    test

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'solucionesfei/test-package',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e49871b6e916e6735e3491482d6cb8ce6d0e0c42',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'solucionesfei/test-package' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e49871b6e916e6735e3491482d6cb8ce6d0e0c42',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
